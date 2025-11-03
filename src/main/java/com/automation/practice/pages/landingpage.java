08b4f2e2071f1755ade5187ac58ce061533547ab
Roshan Ghadge <roshanghadge568@gmail.com>
2025-11-03 15:15:25 +0530

    Second Test Added for landing page by asserting all the header options

--- src/main/java/com/automation/practice/pages/landingpage.java
+++ src/main/java/com/automation/practice/pages/landingpage.java
@@ -1,5 +1,8 @@
 package com.automation.practice.pages;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,12 +20,21 @@ public class landingpage {
     // Object Pool for storing all webelements of landing page
 
     @FindBy(xpath = "//h1[@class='title']")
-    WebElement pageTitle;
+    private WebElement pageTitle;
+
+    @FindBy(css = "div#crosscol div.widget-content ul li")
+    private List<WebElement> widgetOptions;
 
     // Action Methods for landing page
     public String verify_title() {
         String paget_title = pageTitle.getText();
         return paget_title;
+    }
+
+    public List<String> widget_header() {
+        List<String> header_options = widgetOptions.stream().map(WebElement::getText).collect(Collectors.toList());
+
+        return header_options;
     }
 
 }
--- src/test/java/com/automation/tests/landingpageTest.java
+++ src/test/java/com/automation/tests/landingpageTest.java
@@ -1,5 +1,8 @@
 package com.automation.tests;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,6 +23,19 @@ public class landingpageTest extends BaseTest {
     public void verify_page_title() {
         String actual_page_title = lp.verify_title();
         Assert.assertEquals(actual_page_title, "Automation Testing Practice");
+    }
+
+    @Test(priority = 2)
+    public void verify_widget_header() {
+        List<String> actual_header_option = lp.widget_header();
+        List<String> expected_header_option = Arrays.asList("Home", "Udemy Courses", "Online Trainings", "Blog",
+                "PlaywrightPractice");
+
+        Assert.assertEquals(actual_header_option, expected_header_option,
+                "Widget Options are does not matched correctly");
+        for (String option : actual_header_option) {
+            System.out.println("Header Options :- " + option);
+        }
     }
 
 }
